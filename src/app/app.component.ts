2399cc8294af76e2fdf0a1d82dfb7a4c6d53cc6c
laribi.skander <laribi.skander5@gmail.com>
2022-01-14 18:51:36 +0100

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'training-app';
+ // session_name='Formation Web Avancee';
+ firstSession ={
+   id:1,
+   name: 'Formation Web',
+   track: "MEAN Stack",
+   date: new Date('2021-11-25'),
+   local: 'Tunis',
+   duree: 3,
+   participants: 0
+ }
+  
 }
